d4e7fe856d411be253d746da4a5508e348db67f0
ademilsonalmeida <ademilsonalmeida@gmail.com>
2021-01-03 16:13:27 -0300

    Alteração no retorno

--- aalmeidaCICD.API/Controllers/WeatherForecastController.cs
+++ aalmeidaCICD.API/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace aalmeidaCICD.API.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Olá, QA!!!");
+            return Ok("Olá, QA!");
         }
     }
 }
